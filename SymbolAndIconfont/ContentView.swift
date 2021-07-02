59d4e0d6b12d216dbc680f7941d8c38477c5d6b0
张亚飞 <672233580@qq.com>
2021-07-02 15:06:08 +0800

    飞
    修改了哪里

--- SymbolAndIconfont/ContentView.swift
+++ SymbolAndIconfont/ContentView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showSymbol : Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            HStack {
+                
+                Toggle(isOn: $showSymbol) {
+                    
+                    Text(showSymbol ? "Symbol" : "Iconfont")
+                }
+                .padding()
+                
+            }
+            
+            Spacer()
+            
+            
+            if showSymbol {
+                
+                VStack(spacing: 20) {
+                   
+                    Image(systemName: "pencil.tip.crop.circle.badge.minus")
+                        .foregroundColor(.red)
+                    
+                    
+                    Image(systemName: "pencil.tip.crop.circle.badge.minus")
+                        .foregroundStyle(.red, .black)
+                    
+                    Image(systemName: "sunrise.fill")
+                        .foregroundStyle(.red, .black)
+                    
+                    Image(systemName: "moon.zzz.fill")
+                        .foregroundStyle(.red, .black, .orange)
+                    
+                    Image(systemName: "thermometer.sun.fill")
+                        .foregroundStyle(.red, .yellow, .orange)
+                    
+                    Image(systemName: "filemenu.and.selection")
+                        .foregroundStyle(.red, .yellow)
+                }
+            }
+            else {
+                
+                VStack(spacing: 20) {
+                    
+                    Image(systemName: "pencil.tip.crop.circle.badge.minus")
+                        .foregroundColor(.red)
+                    
+                    
+                    Image(systemName: "pencil.tip.crop.circle.badge.minus")
+                        .foregroundStyle(.red, .black)
+                    
+                    Image(systemName: "sunrise.fill")
+                        .foregroundStyle(.red, .black)
+                }
+            }
+            
+           
+            
+            Spacer()
+        }
     }
 }
 
