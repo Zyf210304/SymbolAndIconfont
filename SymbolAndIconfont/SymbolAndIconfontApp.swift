da415e563f43fe9603d55a7d04bf184255253bd8
张亚飞 <672233580@qq.com>
2021-07-02 14:16:17 +0800

    Initial Commit

---
+++ SymbolAndIconfont/SymbolAndIconfontApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolAndIconfontApp.swift
+//  SymbolAndIconfont
+//
+//  Created by 张亚飞 on 2021/7/2.
+//
+
+import SwiftUI
+
+@main
+struct SymbolAndIconfontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
